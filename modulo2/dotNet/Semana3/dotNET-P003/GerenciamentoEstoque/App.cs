49d9530434ecdfe4114548e87eaadcb51d1c60ce
OutLaio <rlaio.ege@gmail.com>
2023-11-28 14:49:06 -0300

    Dia 28/11/2023

--- modulo2/dotNet/Semana3/dotNET-P003/GerenciamentoEstoque/App.cs
+++ modulo2/dotNet/Semana3/dotNET-P003/GerenciamentoEstoque/App.cs
@@ -1,6 +1,13 @@
 ﻿namespace GerenciamentoEstoque;
 
-public class App
-{
+public class App{
+    public App(){
+        
+    }
 
+    public <int, string, int, float> novoProduto(){
+        
+        var produto = (id: 1, nome: "Maçã", qtd: 10, preço: 3.50);
+        return produto;
+    }
 }
