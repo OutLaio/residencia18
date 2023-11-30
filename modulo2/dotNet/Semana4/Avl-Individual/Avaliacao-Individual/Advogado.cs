d215690bae5b1b510fb4b4b51ef65fa3e34c2c6e
Laio Rodrigues <rlaio.ege@gmail.com>
2023-11-30 17:52:10 -0300

    Avl-Individual: Commit 13 Final

--- modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Advogado.cs
+++ modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Advogado.cs
@@ -4,7 +4,7 @@ public class Advogado : Pessoa{
     private string _cna { get; }
 
     public Advogado(string nome, DateTime dtNasc, string cpf, string cna) : base(nome, dtNasc, cpf){
-        _cna = nome;
+        _cna = cna;
     }
 
     public string CNA { get{ return _cna;}}
--- modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/App.cs
+++ modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/App.cs
@@ -4,9 +4,19 @@ namespace Avaliacao;
 public static class App{
     public static void Main(){
         Lista.AdicionarAdvogado("Laio", "06/01/2000", "12345678910", "557");
-        Lista.AdicionarAdvogado("Andre", "13/04/2001", "01234567890", "557");
-        Lista.AdicionarAdvogado("Joao", "28/03/1999", "11122233344", "557");
-        Lista.AdicionarAdvogado("Matheus", "10/03/2001", "78945612301", "557");
+        Lista.AdicionarCliente("Laio", "06/01/2000", "98282697652", "Solteiro", "Engenheiro Civil");
+        Lista.AdicionarAdvogado("Andre", "13/04/2001", "01234567890", "512");
+        Lista.AdicionarCliente("Laio", "06/01/2000", "09867366812", "Solteiro", "Engenheiro Eletricista");
+        Lista.AdicionarAdvogado("Joao", "28/03/1999", "11122233344", "532");
+        Lista.AdicionarCliente("Laio", "06/01/2000", "12418761234", "Solteiro", "Engenheiro Mecânico");
+        Lista.AdicionarAdvogado("Matheus", "10/03/2001", "78945612301", "548");
+
+        Relatorio.AdvogadosPorIdade(18, 35);
+        Relatorio.ClientesPorIdade(18, 35);
+        Relatorio.ClientesPorEstadoCivil("Solteiro");
+        Relatorio.ClientesEmOrdemAlfabetica();
+        Relatorio.ClientesPorProfissao("Engenheiro");
+        Relatorio.AniversariantesDoMes(3);
     }
 
     public static DateTime ConverterStringParaDateTime(string dataString){
