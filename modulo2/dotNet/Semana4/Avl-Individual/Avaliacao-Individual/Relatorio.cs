f67d611cadd21c5da16886d04ab7e1b57ece390b
Laio Rodrigues <rlaio.ege@gmail.com>
2023-11-30 16:59:53 -0300

    Avl-Individual: Commit 9

--- modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Relatorio.cs
+++ modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Relatorio.cs
@@ -4,18 +4,20 @@ public static class Relatorio{
     public static void ExibirRelatorioAdvogado(string titulo, IEnumerable<Advogado> relatorio){
         Console.WriteLine(titulo);
         foreach (var item in relatorio){
-            Console.WriteLine($"");
+            Console.WriteLine($"{item.Nome} - CPF: {item.CPF} - CNA: {item.CNA}");
         }
     }
 
     public static void ExibirRelatorioCliente(string titulo, IEnumerable<Cliente> relatorio){
         Console.WriteLine(titulo);
         foreach (var item in relatorio){
-            Console.WriteLine($"");
+            Console.WriteLine($"{item.Nome} - CPF: {item.CPF} - Profissão: {item.Profissao}");
         }
     }
 
-    public static void porIdade(int idade){
-        var relatorio1 = ListaAdvogados.advogados.Where(a => a.Idade >= idade);
+    public static void AdvogadosPorIdade(int min, int max){
+        var relatorio = ListaAdvogados.advogados.Where(a => a.Idade >= min && a.Idade <= max);
+        ExibirRelatorioAdvogado($"Advogados com idade entre {min} e {max} anos:", relatorio);
     }
+
 }
