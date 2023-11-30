e7ee5bc433bedc257c7d0bff052dd856317355b1
Laio Rodrigues <rlaio.ege@gmail.com>
2023-11-30 15:45:57 -0300

    Avl-Individual: Commit 2

--- modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Pessoa.cs
+++ modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Pessoa.cs
@@ -1,7 +1,26 @@
 namespace Avaliacao;
 
-public class Pessoa{
+public abstract class Pessoa{
     private string _nome { get; }
     private DateTime _dtNasc { get; }
-    private string _cpf { get; }
+    private string _cpf { get; } 
+
+    public Pessoa(string nome, DateTime dtNasc, string cpf){
+        _nome = nome;
+        _dtNasc = dtNasc;
+        _cpf = cpf;
+    }
+
+    public string Nome { get{ return _nome; }}
+
+    public DateTime DtNasc { get{return _dtNasc; }}
+
+    public string CPF { get{return _cpf;}}
+
+    public int Idade { 
+        get{ 
+            DateTime temp = DateTime.Now.AddYears(-this._dtNasc.Year);
+            
+        }
+    }
 }
