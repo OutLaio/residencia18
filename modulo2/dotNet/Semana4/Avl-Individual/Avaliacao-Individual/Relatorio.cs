554cecbdf088a5110cc043e737d0aab5904e92a1
Laio Rodrigues <rlaio.ege@gmail.com>
2023-11-30 17:11:31 -0300

    Avl-Individual: Commit 10

--- modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Relatorio.cs
+++ modulo2/dotNet/Semana4/Avl-Individual/Avaliacao-Individual/Relatorio.cs
@@ -20,4 +20,34 @@ public static class Relatorio{
         ExibirRelatorioAdvogado($"Advogados com idade entre {min} e {max} anos:", relatorio);
     }
 
+    public static void ClientesPorIdade(int min, int max){
+        var relatorio = ListaClientes.clientes.Where(c => c.Idade >= min && c.Idade <= max);
+        ExibirRelatorioCliente($"Clientes com idade entre {min} e {max} anos:", relatorio);
+    }
+
+    public static void ClientesPorEstadoCivil(string estadoCivil){
+        var relatorio = ListaClientes.clientes.Where(c => c.EstadoCivil.Equals(estadoCivil, StringComparison.OrdinalIgnoreCase));
+        ExibirRelatorioCliente($"Clientes com estado civil {estadoCivil}:", relatorio);
+    }
+
+    public static void ClientesEmOrdemAlfabetica(){
+        var relatorio = ListaClientes.clientes.OrderBy(c => c.Nome);
+        ExibirRelatorioCliente("Clientes em ordem alfabética:", relatorio);
+    }
+
+    public static void ClientesPorProfissao(string texto){
+        var relatorio = ListaClientes.clientes.Where(c => c.Profissao.Contains(texto, StringComparison.OrdinalIgnoreCase));
+        ExibirRelatorioCliente($"Clientes cuja profissão contém \"{texto}\":", relatorio);
+    }
+
+    public static void AniversariantesDoMes(int mes){
+        var advogadosRelatorio = ListaAdvogados.advogados.Where(a => a.DtNasc.Month == mes);
+        ExibirRelatorioAdvogado($"Advogados aniversariantes do mês {mes}:", advogadosRelatorio);
+
+        var clientesRelatorio = ListaClientes.clientes.Where(c => c.DtNasc.Month == mes);
+        ExibirRelatorioCliente($"Clientes aniversariantes do mês {mes}:", clientesRelatorio);
+    }
+
+
+
 }
